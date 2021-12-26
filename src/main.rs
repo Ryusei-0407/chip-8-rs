7451680254f508c939b286bddaf7ec2578633d71
Ryusei-0407 <ryu000407@gmail.com>
2021-12-27 03:12:45 +0900

    load a value into a register

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[derive(Debug)]
+struct CPU {
+    current_operation: u16,
+    register: [u8; 2],
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut cpu = CPU {
+        current_operation: 0,
+        register: [0; 2],
+    };
+
+    cpu.current_operation = 0x8014;
+    cpu.register[0] = 5;
+    cpu.register[1] = 10;
+
+    println!("{:?}", cpu);
 }
