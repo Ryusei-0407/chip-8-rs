439e53c4be647548475657cab173ff8565cb8028
Ryusei-0407 <ryu000407@gmail.com>
2021-12-27 04:33:52 +0900

    extracting variables from opcodes

--- src/main.rs
+++ src/main.rs
@@ -29,14 +29,21 @@ impl CPU {
 }
 
 fn main() {
-    let mut cpu = CPU {
-        current_operation: 0,
-        register: [0; 2],
-    };
+    let opcode: u16 = 0x71E4;
 
-    cpu.current_operation = 0x8014;
-    cpu.register[0] = 5;
-    cpu.register[1] = 10;
+    let c = (opcode&0xF000) >> 12;
+    let x = (opcode&0x0F00) >> 8;
+    let y = (opcode&0x00F0) >> 4;
+    let d = (opcode&0x000F) >> 0;
 
-    println!("{:?}", cpu);
+    assert_eq!(c, 0x7);
+    assert_eq!(x, 0x1);
+    assert_eq!(y, 0xE);
+    assert_eq!(d, 0x4);
+
+    let nnn = opcode&0x0FFF;
+    let kk = opcode&0x00FF;
+
+    assert_eq!(nnn, 0x1E4);
+    assert_eq!(kk, 0xE4);
 }
